7dfb73e66003704e8470925bf9df2add38a0def1
migjesus <migjesus21@gmail.com>
2017-05-31 16:16:53 +0100

    reserva

--- ReservasWebApp/src/java/reservas/Reserva.java
+++ ReservasWebApp/src/java/reservas/Reserva.java
@@ -5,12 +5,113 @@ import java.io.Serializable;
 
 
 public class Reserva implements Serializable {
+    
     private String nome;
-    private String email;
-    private String sala;
-    private String local;
-    private String inicio;
-    private String fim;
+    private String nomeEspaco;
+    private String dataInicio;
+    private String dataFim;
+    private int telefone;
+    private double custoEstimado;
+    private int numUtilizadores;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the nomeEspaco
+     */
+    public String getNomeEspaco() {
+        return nomeEspaco;
+    }
+
+    /**
+     * @param nomeEspaco the nomeEspaco to set
+     */
+    public void setNomeEspaco(String nomeEspaco) {
+        this.nomeEspaco = nomeEspaco;
+    }
+
+    /**
+     * @return the dataInicio
+     */
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    /**
+     * @param dataInicio the dataInicio to set
+     */
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    /**
+     * @return the dataFim
+     */
+    public String getDataFim() {
+        return dataFim;
+    }
+
+    /**
+     * @param dataFim the dataFim to set
+     */
+    public void setDataFim(String dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public int getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the custoEstimado
+     */
+    public double getCustoEstimado() {
+        return custoEstimado;
+    }
+
+    /**
+     * @param custoEstimado the custoEstimado to set
+     */
+    public void setCustoEstimado(double custoEstimado) {
+        this.custoEstimado = custoEstimado;
+    }
+
+    /**
+     * @return the numUtilizadores
+     */
+    public int getNumUtilizadores() {
+        return numUtilizadores;
+    }
+
+    /**
+     * @param numUtilizadores the numUtilizadores to set
+     */
+    public void setNumUtilizadores(int numUtilizadores) {
+        this.numUtilizadores = numUtilizadores;
+    }
+    
     
     
     
